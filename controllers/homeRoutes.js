0a34783f0b73fd93c129e380b8076ccdcddae4ae
Nick-McCarthy <mccarthy.nick.m@gmail.com>
2023-02-10 14:00:26 -0600

    new route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+//const { User } = require('../models');
+//const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.render('index.html')
+        console.log('homeRoutes hit')
+        res.render('index')
         /*const userData = await User.findAll({
             attributes: { exclude: ['password'] },
             order: [['name', 'ASC']],
@@ -18,16 +19,25 @@ router.get('/', withAuth, async (req, res) => {
             logged_in: req.session.logged_in,
         });*/
     } catch (err) {
+        console.log('homeRoutes Error Catch')
         res.status(500).json(err);
     }
 });
-
+/*
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/');
         return;
     }
     res.render('login');
+});
+*/
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE vehicle (
     vehicleId INT AUTO_INCREMENT NOT NULL,
     make VARCHAR(30) NOT NULL,
     vehicleModel VARCHAR(30) NOT NULL,
+    milage INT,
     PRIMARY KEY (vehicleId),
     ON DELETE SET NULL
 );
@@ -16,6 +17,7 @@ CREATE TABLE user (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    email varchar(30),
     PRIMARY KEY (id),
     FOREIGN KEY (vehicleId),
     REFERENCES vehicles(vehicleId), 
